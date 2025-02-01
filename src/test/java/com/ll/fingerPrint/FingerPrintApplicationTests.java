0fec6780548c4eacc88cdc973d16cb1bc010d6df
Hjun1004 <hjun1004@naver.com>
2025-02-01 20:38:16 +0900

    create project

---
+++ src/test/java/com/ll/fingerPrint/FingerPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.fingerPrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingerPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
